d5e431692b2cbc67667d90c7947bc96006ecb97d
kaplan hadar <kaplanhadar@gmail.com>
2019-01-16 16:06:39 +0200

    continue

---
+++ src/config.py
@@ -0,0 +1,5 @@
+class Config():
+
+    def __init__(self):
+
+        self.application_key = 'MY_APP_KEY'
--- src/parsy_server.py
+++ src/parsy_server.py
@@ -1,17 +1,10 @@
 from flask import Flask,request,jsonify
 from flask_cors import CORS
-
 from routes import *
 
 app = Flask(__name__)
 CORS(app)
 app.register_blueprint(routes)
-
-# @app.route("/classes/<class_name>",methods=['POST'])
-# def handle_post(class_name):
-#     print(class_name)
-#     print(request.json)
-#     return jsonify({"class_name": class_name}),201
 
 
 @app.route("/",methods=['GET'])
